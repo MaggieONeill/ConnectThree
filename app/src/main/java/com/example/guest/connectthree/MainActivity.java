51439efa7b4dfa131c98a9c0fa82af1cdfff6435
Ariel Spear <arielspear@github.com>
2015-11-10 16:23:51 -0800

    populated board.

--- app/src/main/java/com/example/guest/connectthree/MainActivity.java
+++ app/src/main/java/com/example/guest/connectthree/MainActivity.java
@@ -4,8 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn (View view){
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1000f);
+
+        counter.setImageResource(R.drawable.yellow);
+        counter.animate().translationYBy(1000f).setDuration(300);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,25 +26,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
